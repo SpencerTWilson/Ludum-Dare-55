b7b4548fb4f21ce73dd858eba427172290eb6f5d
SpencerTWilson <50560983+SpencerTWilson@users.noreply.github.com>
2024-04-13 11:56:28 -0500

    stuff

--- scenes/autoloads/demon_manager.gd
+++ scenes/autoloads/demon_manager.gd
@@ -1,4 +1,10 @@
 extends Node
 
+signal scores_updated
+
 var season_count: int = 0
+
+var total_petals: int = 0
+var required_petals_season: int = 10
+
 var extra_flip: bool = false
---
+++ scenes/main_scenes/ScoreCorner.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	DemonManager.scores_updated.connect(_update_displays)
+	_update_displays()
+	
+func _update_displays():
+	$VBoxContainer/ScoreLabel.text = "%d ~ Total Petals" % DemonManager.total_petals
+	$VBoxContainer/QuotaLabel.text = "%d ~ Season Petals" % DemonManager.required_petals_season
+	$VBoxContainer/ProgressBar.max_value = DemonManager.required_petals_season
+	$VBoxContainer/ProgressBar.value = DemonManager.total_petals

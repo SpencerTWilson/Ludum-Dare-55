a21822806de9c2d7d24fc1973d5ffaa7c24ee9e1
Day-Fire <128267461+Day-Fire@users.noreply.github.com>
2024-04-14 17:03:59 -0600

    change seasons

--- scenes/main_scenes/sky.gd
+++ scenes/main_scenes/sky.gd
@@ -2,12 +2,20 @@ extends Control
 
 @export var texture1 : Sprite2D
 @export var texture2 : Sprite2D
+
 @export var Season = 0
-@export var spring : Texture2D
-@export var summer : Texture2D
-@export var fall : Texture2D
-@export var winter : Texture2D
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+@export var spring : Node
+@export var summer : Node
+@export var fall : Node
+@export var winter : Node
+
+@export var spring_tex : Texture2D
+@export var summer_tex : Texture2D
+@export var fall_tex : Texture2D
+@export var winter_tex : Texture2D
+
+
 func _process(delta):
 	texture1.offset -= Vector2(0.1,0)
 	texture2.offset -= Vector2(0.1,0)
@@ -18,14 +26,30 @@ func _process(delta):
 	
 	match Season:
 		0:
-			texture1.texture = spring
-			texture2.texture = spring
+			texture1.texture = spring_tex
+			texture2.texture = spring_tex
+			spring.visible = true
+			summer.visible = false
+			fall.visible = false
+			winter.visible = false
 		1:
-			texture1.texture = summer
-			texture2.texture = summer
+			texture1.texture = summer_tex
+			texture2.texture = summer_tex
+			spring.visible = false
+			summer.visible = true
+			fall.visible = false
+			winter.visible = false
 		2:
-			texture1.texture = fall
-			texture2.texture = fall
+			texture1.texture = fall_tex
+			texture2.texture = fall_tex
+			spring.visible = false
+			summer.visible = false
+			fall.visible = true
+			winter.visible = false
 		3:
-			texture1.texture = winter
-			texture2.texture = winter
+			texture1.texture = winter_tex
+			texture2.texture = winter_tex
+			spring.visible = false
+			summer.visible = false
+			fall.visible = false
+			winter.visible = true

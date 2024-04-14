e559371afc18ca9b0da0c2d00840786fd6a2f89a
SpencerTWilson <50560983+SpencerTWilson@users.noreply.github.com>
2024-04-14 18:02:14 -0500

    font

--- scenes/main_scenes/ScoreCorner.gd
+++ scenes/main_scenes/ScoreCorner.gd
@@ -6,4 +6,4 @@ func _ready():
 	
 func _update_displays():
 	$VBoxContainer/ScoreLabel.text = "%d ~ Total Petals" % (DemonManager.total_petals + DemonManager.season_petals)
-	$VBoxContainer/QuotaLabel.text = "%d ~ Season Petals" % DemonManager.required_petals_season
+	$VBoxContainer/QuotaLabel.text = "%d ~ Required Petals" % DemonManager.required_petals_season

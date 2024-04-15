8230dd6417cbf5edbbd26de8b8b77cd99ee03cae
SpencerTWilson <50560983+SpencerTWilson@users.noreply.github.com>
2024-04-14 19:50:54 -0500

    heyo

--- scenes/change_scene.gd
+++ scenes/change_scene.gd
@@ -5,4 +5,19 @@ extends Button
 func _pressed():
 	AudioManager._play_clip(load("res://assets/sounds/kenney_rpg_audio/Audio/drawKnife1.ogg"),"UI")
 	get_tree().paused = false
+	DemonManager.season_count = 0
+
+	DemonManager.season_petals = 0
+
+	DemonManager.total_petals = 0
+	DemonManager.required_petals_season = 0
+	DemonManager.last_required_petals = 0
+
+	DemonManager.extra_flip = false
+
+	DemonManager.special_summon = false
+	DemonManager.bird_ribbon_gold = false
+	DemonManager.summon_animals = false
+	
+	SlotManager.active_slots.clear()
 	get_tree().change_scene_to_file(scene_path)
